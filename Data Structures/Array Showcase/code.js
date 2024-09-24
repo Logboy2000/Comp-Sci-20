7ca242dade46e6bb66ac0b75cdb957b8c3f2857b
Logan Howarth <loganhowarth@icloud.com>
2024-09-24 11:13:01 -0600

    uouoahrgerg arrayyyyy thingamajig

--- Data Structures/Array Showcase/code.js
+++ Data Structures/Array Showcase/code.js
@@ -5,9 +5,12 @@ var numbers2 = []
 function go() {
     displayArray(names, "output")
     displayArray(numbers, "numbersOutput")
+    displayArray(array1, "array1Display")
+    displayArray(array2, "array2Display")
 }
 
 function displayArray(array, elementId) {
+    document.getElementById(elementId).innerHTML = ""
     for (i = 0; i < array.length; i++) {
         document.getElementById(elementId).innerHTML += array[i] + "<br/> "
     }
@@ -27,20 +30,55 @@ function searchArrayButton() {
 }
 
 
-function copyArray(copiedArray, pasteArray) {
+function copyArray(copiedArray) {
+    var outputArray = []
     var i
     for (i = 0; i < copiedArray.length; i++) {
-        pasteArray[i] = copiedArray[i]
+        outputArray[i] = copiedArray[i]
     }
+    return outputArray
 }
 function copyArrayButton() {
-    copyArray(numbers, numbers2)
+    numbers2 = copyArray(numbers)
+    document.getElementById("numbers2Output").innerHTML = ""
     displayArray(numbers2, "numbers2Output")
 }
 
-function insertIntoArray(array, insertionIndex, value) {
+var arrayThatGetsInsertedInto = [1, 2, 5]
+
+function insertIntoArray(value, insertionIndex, inputArray) {
+    var outputArray = inputArray
     var i
-    for(i = insertionIndex;){
-        
+    for (i = insertionIndex; i < inputArray.length - 1; i++) {
+        outputArray[i + 1] = outputArray[i]
     }
+
+    outputArray[insertionIndex] = value
+    return outputArray
+}
+function insertIntoArrayButton() {
+    arrayThatGetsInsertedInto = insertIntoArray(document.getElementById("arrayInsertionInputValue").value, document.getElementById("arrayInsertionInputIndex").value, arrayThatGetsInsertedInto)
+    displayArray(arrayThatGetsInsertedInto, "arrayThatGetsInsertedIntoOutput")
+}
+
+
+
+
+
+function mergeArrays(array1, array2) {
+    var outputArray = copyArray(array1)
+    var i
+    for (i = 0; i < array2.length; i++) {
+        outputArray[i + array1.length] = array2[i]
+    }
+    return outputArray
+}
+
+
+var array1 = [5, 2, 7]
+var array2 = ["y", "x", "z"]
+function mergeArraysButton(){
+
+    var mergedArray = mergeArrays(array1, array2)
+    displayArray(mergedArray, "mergedArrayDisplay")
 }
