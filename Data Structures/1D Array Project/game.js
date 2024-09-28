b344f2bfefb7b633c8477a8de42d7d3e9611cf7b
Logan Howarth <loganhowarth@icloud.com>
2024-09-27 20:47:46 -0600

    GAME COMPLETE

--- Data Structures/1D Array Project/game.js
+++ Data Structures/1D Array Project/game.js
@@ -1,20 +1,24 @@
 var currentAnswerArray = []
-var currentAnswerString = ''
+var currentGuessArray = []
 
-var currentWordArray = []
+var wrongPicks = 0
 
-var isFetching = false
+
+var gameStates = {
+	PLAYING: 0,
+	FETCHING: 1,
+	ANSWER_SHOWN: 2,
+	GAME_OVER: 3,
+	GAME_WON: 4,
+}
+
+var gameState = gameStates.PLAYING
 
 function go() {
 	generateKeys()
-	fetchRandomWord()
+	randomWordButton()
 }
 
-var twoDee = [
-	[1,2,3],
-	[4,5,6],
-	[7,8,9]
-]
 
 
 function stringToArray(string) {
@@ -28,33 +32,18 @@ function stringToArray(string) {
 }
 
 
-function fetchRandomWord() {
-	var length = document.getElementById('lengthInput').value
-	if (length == '') {
-		length = randomRange(2, 10)
-	}
-	if (!isFetching) {
-		isFetching = true
-		document.getElementById('word').innerHTML = 'Fetching word, please wait'
-		fetch('https://random-word-api.herokuapp.com/word?length=' + length)
-			.then(res => res.json())
-			.then(data => {
-				isFetching = false
-				currentAnswerString = data[0].toUpperCase()
-				currentAnswerArray = stringToArray(currentAnswerString)
-				document.getElementById('word').innerHTML = ''
-				currentWordArray = []
-				for (i = 0; i < currentAnswerArray.length; i++) {
-					currentWordArray[i] = '_'
-				}
-				document.getElementById('word').innerHTML = currentWordArray
-				removeKeys()
-				generateKeys()
-			})
-			.catch(err => {
-				document.getElementById('word').innerHTML = 'Word not found, try another length'
-			})
-	}
+async function fetchRandomWord(length) {
+	document.getElementById('word').innerHTML = 'Fetching word, please wait'
+
+	fetch('https://random-word-api.herokuapp.com/word?length=' + length)
+		.then(res => res.json())
+		.then(data => {
+			changeWord(data[0])
+		})
+		.catch(err => {
+			document.getElementById('word').innerHTML = 'Word not found, try another length'
+		})
+
 }
 
 
@@ -62,24 +51,68 @@ function searchArray(array, valueToFind) {
 	var i
 	for (i = 0; i < array.length; i++) {
 		if (array[i] == valueToFind) {
-			console.log(i)
+			return true
 		}
 	}
-	return
+	return false
+}
+
+function displayArray(array, elementId) {
+	document.getElementById(elementId).innerHTML = ''
+	for (i = 0; i < array.length; i++) {
+		document.getElementById(elementId).innerHTML += ' ' + array[i] + ' '
+	}
 }
 
 function keyPressed(key) {
-	if (!isFetching) {
-		var button = document.getElementById(key)
-		button.style.display = 'none'
-		document.getElementById("guessDisplay").innerHTML = key
+	if (gameState == gameStates.PLAYING) {
+		// Hide button
+		document.getElementById(key).style.display = 'none'
 
+		// Check for game loss
+		var isKeyCorrect = false
 		for (i = 0; i < currentAnswerArray.length; i++) {
 			if (currentAnswerArray[i] == key) {
-				currentWordArray[i] = key
+				currentGuessArray[i] = key
+				isKeyCorrect = true
 			}
 		}
-		document.getElementById('word').innerHTML = currentWordArray
+		if (isKeyCorrect == false) {
+			wrongPicks += 1
+			document.getElementById('hangman').setAttribute('src', 'images/hangman' + wrongPicks + '.png')
+			if (wrongPicks >= 6) {
+				gameState = gameStates.GAME_OVER
+			}
+		}
+		// Check for game won
+		if (searchArray(currentGuessArray, '__') == false) {
+			gameState = gameStates.GAME_WON
+		}
+
+
+
+		switch (gameState) {
+			case gameStates.PLAYING:
+
+				break
+			case gameStates.FETCHING:
+
+				break
+			case gameStates.ANSWER_SHOWN:
+
+				break
+			case gameStates.GAME_OVER:
+				document.getElementById('word').style.color = '#FF0000'
+				break
+			case gameStates.GAME_WON:
+				document.getElementById('word').style.color = '#00FF00'
+				break
+		}
+
+		if (currentGuessArray == currentAnswerArray) {
+			document.getElementById('word').style.color = '#00FF00'
+		}
+		displayArray(currentGuessArray, 'word')
 
 
 
@@ -94,16 +127,12 @@ function removeKeys() {
 	while (keyboardDiv.firstChild) {
 		keyboardDiv.removeChild(keyboardDiv.firstChild)
 	}
-	
+
 }
 function generateKeys() {
 	// Generate keyboard buttons dynamically (wOwIe!!)
-	const keysQwerty = [
-		'Q', 'W', 'E', 'R', 'T', 'Y', 'U', 'I', 'O', 'P',
-		'A', 'S', 'D', 'F', 'G', 'H', 'J', 'K', 'L',
-		'Z', 'X', 'C', 'V', 'B', 'N', 'M'
-	];
 	const keysAlphabetical = [
+		'1','2','3','4','5','6','7','8','9','0',
 		'A', 'B', 'C', 'D', 'E', 'F', 'G', 'H', 'I', 'J',
 		'K', 'L', 'M', 'N', 'O', 'P', 'Q', 'R', 'S', 'T',
 		'U', 'V', 'W', 'X', 'Y', 'Z'
@@ -120,7 +149,17 @@ function generateKeys() {
 		button.id = key
 		// Append created button
 		lettersDiv.appendChild(button)
+		if (key == '0'){
+			lettersDiv.appendChild(document.createElement('br'))
+		}
 	});
+
+	const showAnswerButton = document.createElement('button')
+	showAnswerButton.textContent = 'Show Answer'
+	showAnswerButton.onclick = () => showAnswer()
+	showAnswerButton.id = 'showAnswerButton'
+	lettersDiv.appendChild(showAnswerButton)
+
 }
 
 function randomRange(min, max) {
@@ -128,5 +167,46 @@ function randomRange(min, max) {
 }
 
 function showAnswer() {
-	document.getElementById('word').innerHTML = currentAnswerArray
+	gameState = gameStates.ANSWER_SHOWN
+	displayArray(currentAnswerArray, 'word')
+}
+
+function randomWordButton() {
+	var length = document.getElementById('lengthInput').value
+	if (length == '') {
+		length = randomRange(3, 10)
+	}
+
+	gameState = gameStates.FETCHING
+
+	fetchRandomWord(length)
+
+}
+
+function customWordButton() {
+	if (document.getElementById('customAnswerInput').value == '') { return }
+	changeWord(document.getElementById('customAnswerInput').value)
+	document.getElementById('customAnswerInput').value = ''
+}
+
+
+function changeWord(word) {
+	wrongPicks = 0
+	document.getElementById('hangman').setAttribute('src', 'images/hangman' + wrongPicks + '.png')
+	currentAnswerArray = stringToArray(word.toUpperCase())
+	document.getElementById('word').innerHTML = ''
+	currentGuessArray = []
+	for (i = 0; i < currentAnswerArray.length; i++) {
+		if (currentAnswerArray[i] == ' ') {
+			currentGuessArray[i] = '\xa0'
+		} else {
+			currentGuessArray[i] = '__'
+		}
+
+	}
+	displayArray(currentGuessArray, 'word')
+	removeKeys()
+	generateKeys()
+	document.getElementById('word').style.color = '#FFFFFF'
+	gameState = gameStates.PLAYING
 }
