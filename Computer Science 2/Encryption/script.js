ca723e4daef4e24755ecaa0b963dca7a5cfb22d6
Logan Howarth <loganhowarth@icloud.com>
2024-10-07 11:09:21 -0600

    encryption has begun

---
+++ Computer Science 2/Encryption/script.js
@@ -0,0 +1,21 @@
+var alphabet = []
+for (let i = 0; i < 26; i++) {
+    alphabet.push(String.fromCharCode(65 + i)) // 65 is the ASCII code for 'A'
+}
+
+function encryptString(string){
+    string = string.toUpperCase()
+    
+    
+
+
+    return string
+}
+function decryptString(string){
+    string = string.toUpperCase()
+    
+    
+
+
+    return string
+}
