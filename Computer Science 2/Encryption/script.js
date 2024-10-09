15550326ba9240b48355c98713be56f662553d66
Logan Howarth <loganhowarth@icloud.com>
2024-10-09 11:14:22 -0600

    cookie

--- Computer Science 2/Encryption/script.js
+++ Computer Science 2/Encryption/script.js
@@ -3,19 +3,35 @@ for (let i = 0; i < 26; i++) {
     alphabet.push(String.fromCharCode(65 + i)) // 65 is the ASCII code for 'A'
 }
 
-function encryptString(string){
+var cipher1 = ['Q', 'W', 'E', 'R', 'T', 'Y', 'U', 'I', 'O', 'P', 'L', 'K', 'J', 'H', 'G', 'F', 'D', 'S', 'A', 'Z', 'X', 'C', 'V', 'B', 'N', 'M']
+
+function encryptString(string, cipher) { //Encrypts a string using a polyalphabetic cipher
     string = string.toUpperCase()
-    
-    
+    for (var i = 0; i < string.length; i++) {
+
+        string = string.replaceAt(i, cipher[searchArray(alphabet, string[i])])
+    }
+    return string
+}
+function decryptString(string) {
+    string = string.toUpperCase()
+
+
 
 
     return string
 }
-function decryptString(string){
-    string = string.toUpperCase()
-    
-    
 
+function searchArray(array, valueToFind) {
+    var i
+    for (i = 0; i < array.length; i++) {
+        if (array[i] == valueToFind) {
+            return i
+        }
+    }
+    return -1
+}
 
-    return string
+String.prototype.replaceAt = function (index, replacement) {
+    return this.substring(0, index) + replacement + this.substring(index + replacement.length);
 }
---
+++ Cookie Clicker/script.js
@@ -0,0 +1,9 @@
+var cookies = 0
+
+function go(){
+
+}
+function cookieClicked(){
+    cookies += 1
+    document.getElementById('cookieCount').innerText = cookies
+}
