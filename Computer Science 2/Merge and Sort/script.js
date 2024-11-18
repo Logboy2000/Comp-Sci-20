30e21d9713318ecf9e64f115a85baf20370292a2
Logan Howarth <loganhowarth@icloud.com>
2024-11-18 11:09:47 -0700

    ytjtyj

--- Computer Science 2/Merge and Sort/script.js
+++ Computer Science 2/Merge and Sort/script.js
@@ -2,6 +2,15 @@ var array1 = [1, 60, 5, 7, 9,-10]
 var array2 = [2, 4, 11, 6, 8, 10]
 var arrayMerged = []
 
+//Start of loaded function
+function loaded(){
+    displayArray(array1, "array1")
+    displayArray(array2, "array2")
+}
+//End of loaded function
+
+
+//Start of mergeAndSort function
 function mergeAndSort(array1, array2) {
     // Merge arrays
     var outputArray = []
@@ -11,9 +20,9 @@ function mergeAndSort(array1, array2) {
     for(var i = 0; i < outputArray.length; i++) {
         for(var j = 0; j < outputArray.length - 1; j++) {
             if(outputArray[j] > outputArray[j + 1]) {
-                var temp = outputArray[j]
+                var tempArray = outputArray[j]
                 outputArray[j] = outputArray[j + 1]
-                outputArray[j + 1] = temp
+                outputArray[j + 1] = tempArray
             }
         }
     }
@@ -21,6 +30,17 @@ function mergeAndSort(array1, array2) {
     console.log(outputArray)
     return outputArray
 }
+//End of mergeAndSort function
+
+//Start of button function
 function button(){
-    mergeAndSort(array1, array2)
+    arrayMerged = mergeAndSort(array1, array2)
+    displayArray(arrayMerged, "arrayMerged")
 }
+//End of button function
+
+//Start of displayArray function
+function displayArray(array, elementID){
+    document.getElementById(elementID).innerHTML = array.join(", ")
+}
+//End of displayArray function
--- Data Structures/Connect4/script.js
+++ Data Structures/Connect4/script.js
@@ -11,147 +11,6 @@ const PLAYER_COLORS = [
     '#FFFF00', // Yellow
     '#00FF00', // Green
     '#9400D3', // Violet
-    '#FF7F00', // Orange
-    '#4B0082', // Indigo
-    '#000000', // Black
-    '#00FFFF', // Cyan
-    '#FF00FF', // Magenta
-    '#800080', // Purple
-    '#008000', // Dark Green
-    '#000080', // Navy
-    '#800000', // Maroon
-    '#808000', // Olive
-    '#008080', // Teal
-    '#FFC0CB', // Pink
-    '#FFD700', // Gold
-    '#C0C0C0', // Silver
-    '#808080', // Gray
-    '#A52A2A', // Brown
-    '#DEB887', // Burlywood
-    '#FF69B4', // Hot Pink
-    '#7FFF00', // Chartreuse
-    '#D2691E', // Chocolate
-    '#FF7F50', // Coral
-    '#6495ED', // Cornflower Blue
-    '#DC143C', // Crimson
-    '#00008B', // Dark Blue
-    '#008B8B', // Dark Cyan
-    '#B8860B', // Dark Goldenrod
-    '#A9A9A9', // Dark Gray
-    '#006400', // Dark Green
-    '#BDB76B', // Dark Khaki
-    '#8B008B', // Dark Magenta
-    '#556B2F', // Dark Olive Green
-    '#FF8C00', // Dark Orange
-    '#9932CC', // Dark Orchid
-    '#8B0000', // Dark Red
-    '#E9967A', // Dark Salmon
-    '#8FBC8F', // Dark Sea Green
-    '#483D8B', // Dark Slate Blue
-    '#2F4F4F', // Dark Slate Gray
-    '#00CED1', // Dark Turquoise
-    '#9400D3', // Dark Violet
-    '#FF1493', // Deep Pink
-    '#00BFFF', // Deep Sky Blue
-    '#696969', // Dim Gray
-    '#1E90FF', // Dodger Blue
-    '#B22222', // Firebrick
-    '#228B22', // Forest Green
-    '#FF00FF', // Fuchsia
-    '#DCDCDC', // Gainsboro
-    '#F8F8FF', // Ghost White
-    '#FFD700', // Gold
-    '#DAA520', // Goldenrod
-    '#808080', // Gray
-    '#ADFF2F', // Green Yellow
-    '#F0FFF0', // Honeydew
-    '#FF69B4', // Hot Pink
-    '#CD5C5C', // Indian Red
-    '#4B0082', // Indigo
-    '#FFFFF0', // Ivory
-    '#F0E68C', // Khaki
-    '#E6E6FA', // Lavender
-    '#FFF0F5', // Lavender Blush
-    '#7CFC00', // Lawn Green
-    '#FFFACD', // Lemon Chiffon
-    '#ADD8E6', // Light Blue
-    '#F08080', // Light Coral
-    '#E0FFFF', // Light Cyan
-    '#FAFAD2', // Light Goldenrod Yellow
-    '#D3D3D3', // Light Gray
-    '#90EE90', // Light Green
-    '#FFB6C1', // Light Pink
-    '#FFA07A', // Light Salmon
-    '#20B2AA', // Light Sea Green
-    '#87CEFA', // Light Sky Blue
-    '#778899', // Light Slate Gray
-    '#B0C4DE', // Light Steel Blue
-    '#FFFFE0', // Light Yellow
-    '#00FF00', // Lime
-    '#32CD32', // Lime Green
-    '#FAF0E6', // Linen
-    '#FF00FF', // Magenta
-    '#800000', // Maroon
-    '#66CDAA', // Medium Aquamarine
-    '#0000CD', // Medium Blue
-    '#BA55D3', // Medium Orchid
-    '#9370DB', // Medium Purple
-    '#3CB371', // Medium Sea Green
-    '#7B68EE', // Medium Slate Blue
-    '#00FA9A', // Medium Spring Green
-    '#48D1CC', // Medium Turquoise
-    '#C71585', // Medium Violet Red
-    '#191970', // Midnight Blue
-    '#F5FFFA', // Mint Cream
-    '#FFE4E1', // Misty Rose
-    '#FFE4B5', // Moccasin
-    '#FFDEAD', // Navajo White
-    '#000080', // Navy
-    '#FDF5E6', // Old Lace
-    '#808000', // Olive
-    '#6B8E23', // Olive Drab
-    '#FFA500', // Orange
-    '#FF4500', // Orange Red
-    '#DA70D6', // Orchid
-    '#EEE8AA', // Pale Goldenrod
-    '#98FB98', // Pale Green
-    '#AFEEEE', // Pale Turquoise
-    '#DB7093', // Pale Violet Red
-    '#FFEFD5', // Papaya Whip
-    '#FFDAB9', // Peach Puff
-    '#CD853F', // Peru
-    '#FFC0CB', // Pink
-    '#DDA0DD', // Plum
-    '#B0E0E6', // Powder Blue
-    '#800080', // Purple
-    '#663399', // Rebecca Purple
-    '#FF0000', // Red
-    '#BC8F8F', // Rosy Brown
-    '#4169E1', // Royal Blue
-    '#8B4513', // Saddle Brown
-    '#FA8072', // Salmon
-    '#F4A460', // Sandy Brown
-    '#2E8B57', // Sea Green
-    '#FFF5EE', // Seashell
-    '#A0522D', // Sienna
-    '#C0C0C0', // Silver
-    '#87CEEB', // Sky Blue
-    '#6A5ACD', // Slate Blue
-    '#708090', // Slate Gray
-    '#FFFAFA', // Snow
-    '#00FF7F', // Spring Green
-    '#4682B4', // Steel Blue
-    '#D2B48C', // Tan
-    '#008080', // Teal
-    '#D8BFD8', // Thistle
-    '#FF6347', // Tomato
-    '#40E0D0', // Turquoise
-    '#EE82EE', // Violet
-    '#F5DEB3', // Wheat
-    '#FFFFFF', // White
-    '#F5F5F5', // White Smoke
-    '#FFFF00', // Yellow
-    '#9ACD32',  // Yellow Green
 ]
 
 
@@ -206,7 +65,7 @@ var sndDrop = new Audio('Audio/drop.mp3')
 var sndDraw = new Audio('Audio/rizz.mp3')
 var sndWin = new Audio('Audio/win.mp3')
 
-function go() {
+function go() { // Start of go
     
     // Adds all player options to dropdown
     const playerCount = document.getElementById('playerCount')
@@ -247,9 +106,9 @@ function go() {
     })
     // Start game loop
     setInterval(update, 1000 / FPS)
-}
+}// End of go
 
-function update() {
+function update() { // Start of update
 
 
 
@@ -266,9 +125,9 @@ function update() {
     }
 
 
-}
+}// End of update
 
-function updatePlaying() {
+function updatePlaying() { // Start of updatePlaying
     document.getElementById('gameCanvas').style.cursor = 'pointer'
 
     // Update selected column and row
@@ -282,13 +141,13 @@ function updatePlaying() {
     }
     draw()
 
-}
+}// End of updatePlaying
 var animatedPiece = {
     x: 0,
     y: 0,
     targetY: 0,
 }
-function updatePieceFalling() {
+function updatePieceFalling() { // Start of updatePieceFalling
     document.getElementById('gameCanvas').style.cursor = 'default'
     draw()
     // Update animated piece
@@ -327,9 +186,9 @@ function updatePieceFalling() {
     }
     ctx.globalAlpha = 1.0
     drawCircle(animatedPiece.x, animatedPiece.y, pieceWidth / 2)
-}
+} // End of updatePieceFalling
 
-function updateEnd() {
+function updateEnd() {// Start of updateEnd
     document.getElementById('gameCanvas').style.cursor = 'pointer'
     // Draws board in background
     draw()
@@ -361,8 +220,8 @@ function updateEnd() {
     ctx.fillText(text, canvas.width / 2, canvas.height / 2)
     ctx.fillText('Click to play again', canvas.width / 2, canvas.height-25)
     
-}
-function draw() {
+}// End of updateEnd
+function draw() {// Start of draw
     // Clear Canvas with blue
     ctx.globalAlpha = 1.0
     ctx.fillStyle = '#0000FF'
@@ -394,8 +253,8 @@ function draw() {
         drawCircle((selectedColumn * pieceWidth) + boardXOffset + pieceWidth / 2, selectedRow * pieceWidth + pieceWidth / 2, pieceWidth / 2, true, 1)
     }
 
-}
-function drawCircle(x = 0, y = 0, radius = 10, fill = true, piMult = 2) {
+}// End of draw
+function drawCircle(x = 0, y = 0, radius = 10, fill = true, piMult = 2) { // Start of drawCircle
     ctx.beginPath()
     ctx.arc(x, y, radius, 0, piMult * Math.PI)
     if (fill) {
@@ -403,16 +262,17 @@ function drawCircle(x = 0, y = 0, radius = 10, fill = true, piMult = 2) {
     } else {
         ctx.stroke()
     }
-}
-function placePiece(row, column, player) {
+}// End of drawCircle
+
+function placePiece(row, column, player) { // Start of placePiece
     if (board[column][row] == 0) {
         gameState = gameStates.PIECE_FALLING
         animatedPiece.y = -pieceHeight
         sndDrop.cloneNode().play()
     }
 
-}
-function newGame() {
+}// End of placePiece
+function newGame() {// Start of newGame
     boardWidth = Number(document.getElementById('boardWidth').value)
     boardHeight = Number(document.getElementById('boardHeight').value)
     playerCount = Number(document.getElementById('playerCount').value)
@@ -432,23 +292,23 @@ function newGame() {
     playerTurn = 1
     gameState = gameStates.PLAYING
     resizeCanvas()
-}
+}// End of newGane
 
 
 
-function copyArray(copiedArray) {
+function copyArray(copiedArray) { // start of copyArray
     var outputArray = []
     var i
     for (i = 0; i < copiedArray.length; i++) {
         outputArray[i] = copiedArray[i]
     }
     return outputArray
-}
+}// End of copy array
 /**
  * Checks for a sequence of 'winLength' connected pieces in all directions.
  * Returns the player number if a winner is found, or 0 if no winner is found.
  */
-function checkWin() {
+function checkWin() { //Start of checkWin
     // Check horizontal
     for (var row = 0; row < board[0].length; row++) {
         for (var col = 0; col <= board.length - winLength; col++) {
@@ -515,8 +375,9 @@ function checkWin() {
 
     // No winner
     return 0;
-}
-function checkDraw() {
+}// End of checkWin
+
+function checkDraw() { // End of checkDraw
     for (var i = 0; i < board.length; i++) {
         for (var j = 0; j < board[0].length; j++) {
             if (board[i][j] == 0) {
@@ -525,10 +386,10 @@ function checkDraw() {
         }
     }
     return true
-}
+}// Start of checkDraw
 
 
-function toggleFullscreen() {
+function toggleFullscreen() {// Start of toggleFullscreen
     if (!document.fullscreenElement) {
         if (canvas.requestFullscreen) {
             canvas.requestFullscreen().then(() => {
@@ -546,9 +407,9 @@ function toggleFullscreen() {
             });
         }
     }
-}
+}// End of toggleFullscreen
 
-function resizeCanvas() {
+function resizeCanvas() {//Start of resizeCanvas
     if (document.fullscreenElement) {//With fullscreen
         canvas.width = window.innerWidth
         canvas.height = window.innerHeight
@@ -573,7 +434,7 @@ function resizeCanvas() {
         canvas.style.border = '#0000BB 10px solid'
     }
     
-}
+}// End of resizeCanvas
 
 // Add event listener for fullscreenchange
 document.addEventListener('fullscreenchange', resizeCanvas);
