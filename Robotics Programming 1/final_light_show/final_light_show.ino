612e0e6b9cd996e33449c0bc02e1d0084d488854
Logan Howarth <loganhowarth@icloud.com>
2024-11-27 12:35:11 -0700

    Finally some decent effects and that stupid diagram. i wonder how long github will let me make my commit message. hahahahahahahahahahahahahahahahahahahahahahahahahahahahahahahahaha

--- Robotics Programming 1/final_light_show/final_light_show.ino
+++ Robotics Programming 1/final_light_show/final_light_show.ino
@@ -1,7 +1,12 @@
+// Diagram https://raw.githubusercontent.com/Logboy2000/Comp-Sci-20/refs/heads/main/Robotics%20Programming%201/final_light_show/Star.png
 #include <FastLED.h>
 
-// How many leds in your strip?
 #define NUM_LEDS 94
+
+// First LED is 0
+// Led 94 is cooked
+#define LAST_LED 93
+
 
 #define DATA_PIN 10
 #define CLOCK_PIN 13
@@ -12,24 +17,37 @@ CRGB leds[NUM_LEDS];
 // Colors
 CRGB WHITE = CRGB(255,255,255);
 
+int inner_leds[42] = {9,10,11,12,13,14,71,72,73,74,75,33,34,35,36,37,38,79,80,81,82,57,58,59,60,61,62,63,64,65,66,67,68,69,70,76,77,78,84,83,85,86};
 void setup() {
   Serial.begin(9600);
   FastLED.addLeds<WS2811, DATA_PIN, RGB>(leds, NUM_LEDS);
+  // Turn off all LEDS
   line(0,NUM_LEDS,CRGB(0,0,0));
 }// end of setup
 
 void loop() {
-  int del = 1;
-//  startToEnd(CRGB(255,255,255), del);
-//  startToEnd(CRGB(0,255,255), del);
-//  startToEnd(CRGB(0,0,255), del);
-//  startToEnd(CRGB(0,0,0), del);
-//  endToStart(CRGB(255,0,0), del);
-//  endToStart(CRGB(255,255,0), del);
-//  endToStart(CRGB(255,255,255), del);
-//  endToStart(CRGB(0,0,0), del);
-//    allOutside(CRGB(255,255,255), del);
-  line(93,96,WHITE);
+  int del = 10;
+  int del2 = 100;
+  startToEnd(CRGB(0,0,0), del);
+  startToEnd(CRGB(255,255,255), del);
+  startToEnd(CRGB(0,255,255), del);
+  startToEnd(CRGB(0,0,255), del);
+  startToEnd(CRGB(0,0,0), del);
+  endToStart(CRGB(255,0,0), del);
+  endToStart(CRGB(255,255,0), del);
+  endToStart(CRGB(255,255,255), del);
+  endToStart(CRGB(0,0,0), del);
+  allOutside(CRGB(255,255,255));
+  for(int i = 0; i < 6; i++){
+    allOutside(CRGB(0,255,255));
+    allInside(CRGB(255,255,255));
+    delay(del2);
+    allOutside(CRGB(255,255,255));
+    allInside(CRGB(0,255,255));
+    delay(del2);
+  }
+
+  
 
 }// end of loop
 
@@ -58,22 +76,22 @@ void line(int startIndex, int endIndex, CRGB color) {
 void outerBranch(int index, CRGB color) {
   switch (index) {
     case 0:
-      line(1, 9, color);
+      line(0, 8, color);
       break;
     case 1:
-      line(16, 24, color);
+      line(15, 23, color);
       break;
     case 2:
-      line(25, 33, color);
+      line(24, 32, color);
       break;
     case 3:
-      line(40, 48, color);
+      line(39, 47, color);
       break;
     case 4:
-      line(49, 57, color);
+      line(48, 56, color);
       break;
     case 5:
-      line(88, 96, color);
+      line(87, LAST_LED, color);
       break;
     default:
       line(0, NUM_LEDS, CRGB(255, 255, 255));
@@ -108,14 +126,17 @@ void innerBranch(int index, CRGB color) {
 }// end of innerBranch
 
 
-void allOutside(CRGB color, int delayMilliseconds){
+void allOutside(CRGB color){
   outerBranch(0,color);
   outerBranch(1,color);
   outerBranch(2,color);
   outerBranch(3,color);
   outerBranch(4,color);
   outerBranch(5,color);
-  delay(delayMilliseconds);
+}
+
+void allInside(CRGB color){
+  arrayToLights(inner_leds, 42, color);
 }
 
 
@@ -125,4 +146,5 @@ void arrayToLights(int arr[], int arrayLength, CRGB color) {
     int lightIndex = arr[i];
     leds[lightIndex] = color;
   }
+  FastLED.show();
 }
